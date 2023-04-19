8649f5750875116fbdc08a103874e6c60095b896
Daniil Karlov <dskarlov@icloud.com>
2023-04-19 23:28:43 +0300

    Add skills.py

--- skills.py
+++ skills.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from unit import BaseUnit
 
+
 class Skill(ABC):
     """
     Базовый класс умения
@@ -47,22 +48,24 @@ class Skill(ABC):
 
 
 class FuryPunch(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = 'Свирепый пинок'
+    stamina = 6
+    damage = 12
 
     def skill_effect(self):
-        # TODO логика использования скилла -> return str
-        # TODO в классе нам доступны экземпляры user и target - можно использовать любые их методы
-        # TODO именно здесь происходит уменшение стамины у игрока применяющего умение и
-        # TODO уменьшение здоровья цели.
-        # TODO результат применения возвращаем строкой
-        pass
+        self.user.stamina -= self.stamina
+        self.target.get_damage(self.damage)
+
+        return f'{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику.'
+
 
 class HardShot(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = 'Мощный укол'
+    stamina = 5
+    damage = 15
 
     def skill_effect(self):
-        pass
+        self.user.stamina -= self.stamina
+        self.target.get_damage(self.damage)
+
+        return f'{self.user.name} использует {self.name} и наносит {self.damage} урона сопернику.'
